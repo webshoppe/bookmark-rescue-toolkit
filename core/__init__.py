8df268c36d9e92e937d49e3ca1475710fba380f0
webShoppe <webshoppe.ca@gmail.com>
2026-05-01 22:33:50 -0700

    Create __init__.py
    Initial release v1.0.0

---
+++ core/__init__.py
@@ -0,0 +1,12 @@
+# core/__init__.py
+# Intentionally empty - import directly from the individual modules:
+#
+#   from core.version            import __version__
+#   from core.retriever          import recover_bookmarks
+#   from core.json_to_html       import convert_json_to_html
+#   from core.sqlite_to_html     import convert_sqlite_to_html
+#   from core.vault_builder      import build_vault
+#   from core.bookmark_merger    import merge_sources, export_to_csv, export_to_json
+#   from core.bookmark_merger    import sources_from_manifest
+#   from core.search             import search_bookmarks, search_from_raw
+#   from core.archive_beautifier import build_archive
